7ada79aed066ded969d5365c4157d44baae393d9
Siulppe King <elchancateclas@gmail.com>
2026-06-02 08:10:33 -0500

    Set PrivateRouter & add links Index

--- src/routers/PrivateRouter.jsx
+++ src/routers/PrivateRouter.jsx
@@ -1,6 +1,10 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
 
 export const PrivateRouter = () => {
-    
+
+    const isAuthenticated = false;
+
+    if (!isAuthenticated) return <Navigate to="/login" replace />
+
     return <Outlet />
 }
